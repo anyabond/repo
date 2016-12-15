d2859a6fffd0c911f04df5b0c6c0ac01f2fe5f18
anastasiyaoz <nasty---1998@mail.ru>
2016-12-15 03:21:53 +0300

    Added login/register window

---
+++ ConcertAdvicer/ConcertAdvicerApp/Screens/ContentWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Logic.Database;
+
+namespace ConcertAdvicerApp.Screens
+{
+    /// <summary>
+    /// Логика взаимодействия для ContentWindow.xaml
+    /// </summary>
+    public partial class ContentWindow : Window
+    {
+        public Repository Repository { private get; set; }
+        public ContentWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ConcertAdvicer/ConcertAdvicerApp/Screens/LoginRegister.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Logic.Database;
+
+namespace ConcertAdvicerApp.Screens
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginRegister.xaml
+    /// </summary>
+    public partial class LoginRegister : Window
+    {
+        private Repository rep { get; }
+        public LoginRegister()
+        {
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            rep = new Repository();
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginTextBox.Text;
+            string password = passwordBox.Password;
+            try
+            {
+                rep.Login(login, password);
+            }
+            catch (Exception ex)
+            {
+                loginErrorLabel.Content = ex.Message;
+                return;
+            }
+
+            ContentWindow cw = new ContentWindow();
+            cw.Repository = rep;
+            cw.Show();
+            this.Close();
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (rep.LoginUsed(newLoginTextBox.Text))
+            {
+                registerErrorLabel.Content = "Login used!";
+                return;
+            }
+
+            rep.Register(nameTextBox.Text,
+                surnameTextBox.Text,
+                newLoginTextBox.Text,
+                newPasswordBox.Password,
+                emailTextBox.Text);
+            rep.Login(newLoginTextBox.Text, newPasswordBox.Password);
+            ContentWindow cw = new ContentWindow();
+            cw.Repository = rep;
+            cw.Show();
+            this.Close();
+        }
+    }
+}
