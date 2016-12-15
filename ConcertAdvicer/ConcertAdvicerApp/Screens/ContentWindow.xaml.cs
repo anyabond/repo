2e0c61d936ef0c53e78ee78646215a21f5862436
anastasiyaoz <nasty---1998@mail.ru>
2016-12-15 21:49:55 +0300

    Added some UI interactions

--- ConcertAdvicer/ConcertAdvicerApp/Screens/ContentWindow.xaml.cs
+++ ConcertAdvicer/ConcertAdvicerApp/Screens/ContentWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +22,60 @@ namespace ConcertAdvicerApp.Screens
     public partial class ContentWindow : Window
     {
         public Repository Repository { private get; set; }
+
         public ContentWindow()
         {
             InitializeComponent();
+        }
+
+        private void LogoutMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            LoginRegister lr = new LoginRegister();
+            lr.Show();
+            this.Close();
+        }
+
+        private void UpdateDBMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            Thread back = new Thread(Repository.UpdateDb);
+            back.Start();
+            MessageBox.Show("Updating database");
+            back.Join();
+            MessageBox.Show("Database updated!");
+        }
+
+
+        private void CloseMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WishlistDataGrid.ItemsSource = Repository.UserWishlist();
+        }
+
+        private void browse_Loaded(object sender, RoutedEventArgs e)
+        {
+            cityComboBox.ItemsSource = Repository.GetCityShortingDictionary().Keys.ToList();
+            fromDatePicker.SelectedDate = DateTime.Now;
+            toDatePicker.SelectedDate = DateTime.Now.AddMonths(1);
+        }
+
+        private void browseShow_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dictionary = Repository.GetCityShortingDictionary();
+            if (!dictionary.ContainsKey(cityComboBox.Text))
+                return;
+            BrowseDataGrid.ItemsSource = Repository.CityConcerstBetweenDates(
+                dictionary[cityComboBox.Text],
+                fromDatePicker.SelectedDate.Value,
+                toDatePicker.SelectedDate.Value);
+            BrowseDataGrid.Columns.ToList().Where(c =>
+                c.Header.Equals("ID") ||
+                c.Header.Equals("Description") ||
+                c.Header.Equals("Location") ||
+                c.Header.Equals("URL")).ToList().ForEach(column => column.Visibility = Visibility.Hidden);
         }
     }
 }
